9ef5acf02db4be0421ade9cd388e8012b309e5b8
Greyward <32356957+JimmyVictor@users.noreply.github.com>
2019-02-04 15:38:53 +0000

    UDP & TCP Server

---
+++ ConsoleApp1/CSServer/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CSServer
+{
+    class Server
+    {
+        public static string data;
+
+        public static void StartListening()
+        {
+            byte[] bytes = new Byte[1024];
+
+            IPHostEntry iPHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress iPAddress = IPAddress.Loopback;
+            IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, 1234);
+
+            Socket ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            try
+            {
+                ServerSocket.Bind(iPEndPoint);
+                ServerSocket.Listen(10);
+
+                while(true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    Socket ClientSocket = ServerSocket.Accept();
+
+                    data = null;
+
+                    int bytseRec = ClientSocket.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytseRec);
+
+                    Console.WriteLine("Text received: {0}", data);
+
+                    data += "   this is from the server!!!";
+
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    ClientSocket.Send(msg);
+                    ClientSocket.Shutdown(SocketShutdown.Both);
+                    ClientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("Press enter to continue...");
+            Console.Read();
+        }
+
+        public static void StartReceiving()
+        {
+            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress iPAddress = IPAddress.Loopback;
+            IPEndPoint endPoint = new IPEndPoint(iPAddress, 1234);
+
+            Socket s = new Socket(endPoint.Address.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint senderRemote = (EndPoint)sender;
+
+            s.Bind(endPoint);
+
+            byte[] msg = new Byte[256];
+            Console.WriteLine("Waiting to receive datagrams from client...");
+
+            s.ReceiveFrom(msg, msg.Length, SocketFlags.None, ref senderRemote);
+            string result = System.Text.Encoding.UTF8.GetString(msg);
+            Console.WriteLine(result);
+            s.Close();
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            //StartListening();
+            StartReceiving();
+        }
+    } // end of class
+} // end of name space
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Client
+    {
+        public static void StartClient()
+        {
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                IPHostEntry iPHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress iPAddress = IPAddress.Loopback;
+                IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, 1234); // the server we want to connect to
+
+                Socket senderSocket = new Socket(iPAddress.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+
+                try
+                {
+                    senderSocket.Connect(iPEndPoint);
+                    Console.WriteLine("Socket connected to {0}", senderSocket.RemoteEndPoint.ToString());
+
+                    string sMessage = Console.ReadLine();
+
+                    byte[] msg = Encoding.ASCII.GetBytes(sMessage);
+
+                    int bytesSent = senderSocket.Send(msg); // double check message before it is received
+
+                    //int bytesRec = senderSocket.Receive(bytes); // populate the array with data it receives from the server
+                    //Console.WriteLine("Enchoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    senderSocket.Shutdown(SocketShutdown.Both);
+                    senderSocket.Close();
+                }
+                catch (ArgumentException ane)
+                {
+                    Console.WriteLine("ArgumentNullException: {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException: {0}", se.ToString());
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Unexpected exception: {0}", e.ToString());
+                }
+            }// end of first try block
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception: {0}", e.ToString());
+            }
+            //StartClient();
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                StartClient();
+            }
+
+            Console.WriteLine("Client shuting down.");
+            Console.ReadLine();
+        }
+    }// end of class
+}// end of namespace
